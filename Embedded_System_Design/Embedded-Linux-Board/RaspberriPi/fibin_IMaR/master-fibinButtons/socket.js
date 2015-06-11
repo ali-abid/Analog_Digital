497480289e1c0392e02a09d9153b2526b5e3412c
ali-abid <worktimee@gmail.com>
2015-06-11 16:27:26 +0100

    socket js epeask line

--- Embedded_System_Design/Embedded-Linux-Board/RaspberriPi/fibin_IMaR/master-fibinButtons/socket.js
+++ Embedded_System_Design/Embedded-Linux-Board/RaspberriPi/fibin_IMaR/master-fibinButtons/socket.js
@@ -30,32 +30,32 @@ b4.watch(function(err, value) {
     if (err) exit();
     io.sockets.emit('b4', {v:value});
 	echo('inside b4 function');
-    exec('espeak -v en "Button 4"');
-    exec('espeak -v en "Right Leg"');
+    exec('espeak -v en "Button 4 Right Leg"');
+    
 });
 
 b3.watch(function(err, value){
 	if (err) exit();
 	io.sockets.emit('b3', {v:value});
 	echo('inside b3 function');
-	exec('espeak -v en "Button 3"');
-	exec('espeak -v en "Left Leg"');
+	exec('espeak -v en "Button 3 Left Leg"');
+	
 });
 
 b2.watch(function(err, value){
 	if (err) exit();
 	io.sockets.emit('b2' , {v:value});
 	echo('inside b2 function');
-    exec('espeak -v en "Button 2"');
-    exec('espeak -v en "Right Hand"');
+    exec('espeak -v en "Button 2 Right Hand"');
+    
 });
 
 b1.watch(function(err, value){
 	if (err) exit();
 	io.sockets.emit('b1' , {v:value});
 	echo('inside b1 function');
-    exec('espeak -v en "Button 1"');
-    exec('espeak -v en "Left Hand"');
+    exec('espeak -v en "Button 1 Left Hand"');
+ 
 });
 
 function exit() {
